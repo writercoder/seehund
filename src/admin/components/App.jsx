e50740bceb9a2f3826f00814d03e484afcc07f9e
Richard Garbutt <richard@1000mileweb.com>
2017-03-06 15:49:11 +0100

    Install react router

--- src/admin/components/App.jsx
+++ src/admin/components/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 export default class App extends React.Component {
   render() {
     return (
