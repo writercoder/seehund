fd71e26545d55ba6839ff96f1e19652bb4a2cfd0
Richard Garbutt <richard@1000mileweb.com>
2017-03-26 16:07:12 +0200

    Finish posts API

--- api/posts.js
+++ api/posts.js
@@ -8,6 +8,9 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 export function create(event, context, callback) {
   const data = JSON.parse(event.body);
 
+  // const post = await db.createPost(data);
+  // await builder.buildPost(post)
+
   const params = {
     TableName: 'postsTable',
     Item: {
@@ -22,12 +25,16 @@ export function create(event, context, callback) {
     if (error) {
       fail(callback);
     } else {
-      succeed(callback, params.Item);
+      succeed(params.Item, callback);
     }
   })
 }
 
 export function show(event, context, callback) {
+
+  // const post = await db.find(even.pathParameters.id);
+  // succeed(post, callback);
+
   const params = {
     TableName: 'postsTable',
     Key: {
@@ -54,6 +61,46 @@ export function list(event, context, callback) {
       fail(callback);
     } else {
       succeed(data.Items, callback);
+    }
+  })
+}
+
+export function update(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: 'postsTable',
+    Key: {
+      id: event.pathParameters.id
+    },
+    UpdateExpression: 'SET title = :title, content = :content',
+    ExpressionAttributeValues: {
+      ':title': data.title ? data.title : null,
+      ':content': data.content ? data.content : null,
+    },
+    ReturnValues: 'ALL_NEW'
+  }
+
+  dynamoDb.update(params, (error, data) => {
+    if(error) {
+      fail(callback);
+    } else {
+      succeed(data.Attributes, callback);
+    }
+  })
+}
+
+export function destroy(event, context, callback) {
+  const params = {
+    TableName: 'postsTable',
+    Key: {
+      id: event.pathParameters.id
+    }
+  }
+  dynamoDb.delete(params, (error, data) => {
+    if(error) {
+      fail(callback);
+    } else {
+      succeed({status: true},  callback);
     }
   })
 }
