af4f5b3dce8cf2248be6866721e9fbebd315f49c
Richard Garbutt <richard@1000mileweb.com>
2017-04-06 09:07:00 +0200

    Wire up new posts page to create the post

--- client/admin/components/posts/NewPostPage.jsx
+++ client/admin/components/posts/NewPostPage.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import PostForm from './PostForm.jsx'
+import {observer, inject} from 'mobx-react'
+import { withRouter } from 'react-router'
 
+@inject("postsStore") @observer @withRouter
 export default class NewPostsPage extends React.Component {
+
+  createPost = (postFields) => {
+    this.props.postsStore.createPost(postFields, (post) => {
+      this.props.history.push(`${post.id}`)
+    });
+  }
 
   render() {
 
     return (
       <div>
         <h1>Create Post</h1>
-        <PostForm onSubmit={ console.info } />
+        <PostForm onSubmit={ this.createPost } />
       </div>
     )
   }
