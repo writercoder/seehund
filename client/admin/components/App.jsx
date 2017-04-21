7a5d3634c8794bf58706d6fdc092a30b0ea1c096
Richard Garbutt <richard@1000mileweb.com>
2017-04-21 11:13:30 +0200

    Basic app layout #3

--- client/admin/components/App.jsx
+++ client/admin/components/App.jsx
@@ -4,37 +4,24 @@ import {
   Route,
   Link
 } from 'react-router-dom'
+
+import App from 'grommet/components/App';
 import {observer, inject} from "mobx-react";
 import DevTools from 'mobx-react-devtools'
+import Dashboard from './dashboard/Dashboard.jsx'
 import LoginPage from './user/LoginPage.jsx'
-import UserWidget from './user/UserWidget.jsx'
 import PostsIndexPage from './posts/PostsIndexPage.jsx'
 import NewPostPage from './posts/NewPostPage.jsx'
 import PostPage from './posts/PostPage.jsx'
 
-const Dashboard = () => (
-  <div>
-    <h2>Dashboard</h2>
-    <p>Welcome to your blog</p>
-  </div>
-)
-
 
 @observer
-export default class App extends React.Component {
+export default class extends React.Component {
   render() {
 
     return (
       <Router basename="/admin">
-        <div>
-          <ul>
-            <li><Link to="/">Dashboard</Link></li>
-            <li><Link to="/posts">Posts</Link></li>
-            <li><Link to="/posts/new">New Post</Link></li>
-          </ul>
-
-          <UserWidget />
-
+        <App centered={false}>
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/" component={Dashboard} />
           <Route exact path="/posts/:id" render={(props) => {
@@ -47,7 +34,7 @@ export default class App extends React.Component {
           <Route exact path="/posts"
                  render={(props) => <PostsIndexPage {...props} /> } />
           <DevTools />
-        </div>
+        </App>
       </Router>
     )
   }
---
+++ client/admin/components/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DefaultLayout from '../layouts/DefaultLayout.jsx';
+
+export default class extends React.Component {
+  render() {
+    return (
+      <DefaultLayout>
+        <p>Welcome to your blog</p>
+      </DefaultLayout>
+    );
+  }
+}
