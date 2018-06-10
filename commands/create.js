6fe818fae457a3829de880738a8ffab7129ac72f
Richard Garbutt <richard@1000mileweb.com>
2018-06-10 11:03:37 +0200

    CLI command to create cloudformation stack

---
+++ commands/create.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const AWS = require('aws-sdk');
+
+
+const createStack = ({
+  stackName,
+  bucketName,
+  region
+}, callback) => {
+  const cloudformation = new AWS.CloudFormation({region});
+  const templateBody = fs.readFileSync(path.join(__dirname, '../cloudformation/cloudblog.yml'), 'utf8')
+
+  console.info(templateBody)
+
+  cloudformation.createStack({
+    StackName: stackName,
+    TemplateBody: templateBody,
+    Parameters: [{
+      ParameterKey: "WebBucketName",
+      ParameterValue: bucketName
+    }]
+  }, callback)
+};
+
+
+const create = ({
+  title,
+  stackName,
+  bucketName,
+  region
+}) => {
+  createStack({
+    stackName,
+    bucketName,
+    region
+  }, (err, data) => {
+    if(err) {
+      console.log('Error creating stack');
+      console.info(err)
+    } else {
+      console.log(`Created stack with id ${data.StackId}`)
+    }
+  });
+};
+
+
+module.exports = create;
