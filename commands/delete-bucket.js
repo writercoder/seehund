24aa831a9de5b8e6343d647796a9f313f49295ea
Richard Garbutt <richard@1000mileweb.com>
2018-06-21 10:32:37 +0200

    Add a destroy bucket command to destroy the web bucket.

---
+++ commands/delete-bucket.js
@@ -0,0 +1,24 @@
+const { getCoreStackConfig } = require('./get-config');
+const { execFileSync } = require('child_process');
+
+
+const deleteBucket = ({blogName, region}, callback) => {
+  getCoreStackConfig({blogName, region}, (err, data) => {
+    if(err) return callback(err);
+
+    const bucketName = data.SeeBlogWebBucketName;
+
+    const args = [
+      's3',
+      'rb',
+      '--force',
+      `s3://${bucketName}`
+    ];
+
+  console.log(execFileSync('aws', args).toString());
+  callback(null)
+  });
+}
+
+
+module.exports = deleteBucket;
