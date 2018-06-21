564bcad13e50f5d523be4ad965f9e70f08255cbe
Richard Garbutt <richard@1000mileweb.com>
2018-06-21 10:49:08 +0200

    Added a destroy API command

---
+++ commands/destroy-api.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { execFileSync } = require('child_process');
+
+const destroyApi = ({region, blogName}, callback) => {
+
+  const serverlessBin = path.join(
+    __dirname, '../node_modules/serverless/bin/serverless')
+
+  const args = ['remove', '--region', region];
+  const env = Object.assign({}, process.env, { SEEHUND_BLOG: blogName })
+
+  console.log(execFileSync(serverlessBin, args, { env }).toString());
+  callback(null);
+};
+
+
+module.exports = destroyApi;
