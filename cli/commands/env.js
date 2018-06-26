57f017ec58441afa11145bfa0b10fc6e4079e072
Richard Garbutt <richard@1000mileweb.com>
2018-06-26 12:57:34 +0200

    Fix export of SEEHUND_BLOG in env command

--- cli/commands/env.js
+++ cli/commands/env.js
@@ -3,7 +3,7 @@ const { loadBlog } = require('../lib/blog');
 const env = ({blogName, region}) => {
 
   loadBlog({name: blogName, region}, (err, blog) => {
-    console.log(`SEEHUND_BLOG=${blog.name}
+    console.log(`export SEEHUND_BLOG=${blog.name}
 export SEEHUND_WEB_BUCKET=${blog.webBucketName}
 export SEEHUND_USER_POOL_ARN=${blog.adminUserPoolArn}
 export SEEHUND_BLOG_API=${blog.blogApiUrl}
