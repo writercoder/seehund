ac3334e64bb0e2bab769a206c9c6f48d3ce1210e
Richard Garbutt <richard@1000mileweb.com>
2018-06-26 11:52:08 +0200

    Actually export stuff in env command

--- cli/commands/env.js
+++ cli/commands/env.js
@@ -4,9 +4,11 @@ const env = ({blogName, region}) => {
 
   loadBlog({name: blogName, region}, (err, blog) => {
     console.log(`SEEHUND_BLOG=${blog.name}
-SEEHUND_WEB_BUCKET=${blog.webBucketName}
-SEEHUND_USER_POOL_ARN=${blog.adminUserPoolArn}
-SEEHUND_BLOG_API=${blog.blogApiUrl}`)
+export SEEHUND_WEB_BUCKET=${blog.webBucketName}
+export SEEHUND_USER_POOL_ARN=${blog.adminUserPoolArn}
+export SEEHUND_BLOG_API=${blog.blogApiUrl}
+export SEEHUND_APP_CLIENT_ID=${blog.adminAppClientId}
+export SEEHUND_USER_POOL_ID=${blog.adminUserPoolId}`)
   });
 
 };
