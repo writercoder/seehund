1192ad4b438885c1d9ebf1da8811ecd6b60da450
Richard Garbutt <richard@1000mileweb.com>
2018-06-26 11:25:39 +0200

    Output blog api to environment

--- cli/commands/env.js
+++ cli/commands/env.js
@@ -1,13 +1,12 @@
 const { loadBlog } = require('../lib/blog');
 
-
-
 const env = ({blogName, region}) => {
 
   loadBlog({name: blogName, region}, (err, blog) => {
     console.log(`SEEHUND_BLOG=${blog.name}
 SEEHUND_WEB_BUCKET=${blog.webBucketName}
-SEEHUND_USER_POOL_ARN=${blog.adminUserPoolArn}`)
+SEEHUND_USER_POOL_ARN=${blog.adminUserPoolArn}
+SEEHUND_BLOG_API=${blog.blogApiUrl}`)
   });
 
 };
