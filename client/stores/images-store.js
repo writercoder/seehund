0be3ee8237413fbb5e1d5681f83175c9f7c1d718
Richard Garbutt <richard@1000mileweb.com>
2018-11-21 22:16:26 +0100

    Store to handle AWS image uploads

---
+++ client/stores/images-store.js
@@ -0,0 +1,58 @@
+import {action, observable, observe, runInAction} from "mobx";
+import AWS from 'aws-sdk';
+
+import config from './../config.js'
+
+AWS.config.update({
+  region: 'us-east-1',
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: config.cognito.idPoolId
+  })
+});
+
+const IMAGE_PATH = 'content/images';
+
+export class ImagesStore {
+
+  @observable images = [];
+
+  s3() {
+    return new AWS.S3({
+      params: {
+        Bucket: config.webBucket
+      }
+    });
+  }
+
+  @action fetch() {
+    const params = {
+      Delimeter: IMAGE_PATH
+    };
+
+    this.s3().listObjects(params, (err, data) => {
+      runInAction("Updating images from server response", () => {
+        this.images.push(data)
+      });
+    });
+  }
+
+  @action uploadImage(file) {
+    console.log('uploading image')
+    console.log(file);
+    console.info(config);
+    this.s3().upload({
+      Key: `${IMAGE_PATH}/${file.name}`,
+      Body: file,
+      ACL: 'public-read',
+      IdentityPoolId: config.cognito.idPoolId
+    }, (err, data) => {
+      if(err) {
+        console.log(err);
+      } else {
+        runInAction("Updating images after upload", () => {
+          this.images.push(data);
+        });
+      }
+    });
+  }
+}
