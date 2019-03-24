a61153180f40c6f00c68a80fc16c6e68b4a70d7a
Richard Garbutt <richard@1000mileweb.com>
2019-03-24 20:50:44 +0100

    Command to load in sample blog content

---
+++ cli/commands/load-sample-content.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const posts = require('../../lib/blog/posts');
+
+
+const sampleContentDir = path.join(__dirname, '..', '..', 'data', 'sample_content');
+
+const readSampleFile = fileName => fs.readFileSync(path.join(sampleContentDir, fileName), 'utf8');
+
+const insertSampleFile = async ({title, fileName, postsTableName}) => {
+  await posts.create({
+    postsTableName, 
+    title,
+    content: readSampleFile(fileName)
+  });
+}
+
+async function loadSampleContent({blogName, region}) {
+
+  const postsTableName = `${blogName}-postsTable`;
+
+  await insertSampleFile({
+    postsTableName,
+    title: 'Welcome to your Seehund blog', 
+    fileName: 'welcome.md'
+  });
+  await insertSampleFile({
+    postsTableName,
+    title: 'Seehund Credits',
+    fileName: 'credits.md'
+  });
+}
+
+module.exports = loadSampleContent;
