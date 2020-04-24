0920f8334296eb0087444cff120e5cbb88a85f80
Richard Garbutt <richard@1000mileweb.com>
2020-04-24 06:38:23 +0200

    Don't use offline dynamodb in CI

--- test/globalTeardown.js
+++ test/globalTeardown.js
@@ -1,8 +1,5 @@
 const { teardown: teardownDevServer } = require('jest-dev-server')
 
 module.exports = async function globalTeardown() {
-  if(process.env.CI) {
-    return
-  }
   await teardownDevServer()
 }
