47a8e0d209f432f6a73d045434de6182cbe0ab74
Richard Garbutt <richard@1000mileweb.com>
2020-06-28 14:51:43 +0200

    Use global decorator for grommet config

--- client/admin/components/layouts/DefaultLayout/DefaultLayout.stories.jsx
+++ client/admin/components/layouts/DefaultLayout/DefaultLayout.stories.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 
 import DefaultLayout from './DefaultLayout'
-import { Grommet, grommet } from 'grommet'
-
-const grommetDecorator = storyFn => <Grommet full theme={grommet}>{storyFn()}</Grommet>
 
 export default {
   title: "DefaultLayout",
   component: DefaultLayout,
-  decorators: [grommetDecorator]
 }
 
 export const Default = () => <DefaultLayout title="My blog about clouds"><p>Content goes here</p></DefaultLayout>
