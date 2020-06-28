3f7cc9004279caa65bb0612917a0d458cb8ce073
Richard Garbutt <richard@1000mileweb.com>
2020-06-28 15:50:33 +0200

    Install and configure storybook-react-router to use router components in stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import { addDecorator } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
 
 import grommetDecorator from './grommetDecorator'
 
+addDecorator(StoryRouter())
 addDecorator(grommetDecorator)
